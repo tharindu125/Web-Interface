ae56fbf7172040c5d02a9522b2f901c0be8bfced
tharindu125 <nawanjanaenda@gmail.com>
2022-09-21 23:59:35 +0530

    Add changes

---
+++ src/components/Navbar/Menuiitems.js
@@ -0,0 +1,32 @@
+const menuitems = [
+    {
+        tittle: 'Home',
+        url: '#',
+        cName: 'nab_links'
+
+    },
+    {
+        tittle: 'Services',
+        url: '#',
+        cName: 'nab_links'
+
+    },
+    {
+        tittle: 'Products',
+        url: '#',
+        cName: 'nab_links'
+
+    },
+    {
+        tittle: 'Contact Us',
+        url: '#',
+        cName: 'nab_links'
+
+    },
+    {
+        tittle: 'Sign up',
+        url: '#',
+        cName: 'nab_links'
+
+    },
+]
